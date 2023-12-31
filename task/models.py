a756b16357d79d42de8835c52cf18251248404ee
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-31 22:09:23 +0300

    🌈 Added TaskPriority model for task categorization. 🚧 Implemented TaskPriority in TaskModel for better organization. 📝 Updated task_description field to allow longer details. 💡 Enhanced TaskModel by integrating TaskCategory functionalities. 🔄 Refactored TaskCategory for improved code structure.

--- task/models.py
+++ task/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class TaskCategory(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+class TaskModel(models.Model):
+    task_title = models.CharField(max_length=255)
+    task_description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    task_assign_date = models.DateField()
+    categories = models.ManyToManyField(TaskCategory)
+
+    def __str__(self):
+        return self.taskTitle
