d8a4d16663767eb634c5324c1bcad7a7aed3b146
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-31 21:53:51 +0300

    🌐 Added navigation bar with Home link. 📝 Created Task Form link in the navigation bar. ➕ Integrated Category Form link in the navigation bar. 🔍 Implemented search bar with Bootstrap styling. 🔧 Fixed setup for Bootstrap in myproject/views.py.

--- category/urls.py
+++ category/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import category_form
 
 urlpatterns = [
-    path('form/', category_form, name='category_form'),
+    path('', category_form, name='category_form'),
     # Add more URL patterns as needed
 ]
